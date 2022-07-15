e3588e5acd988cd2c39b91319e1ca065ab588e38
InsidTrifl <InsidTrifl@proton.me>
2022-07-15 14:36:12 +0600

    First commit of the working weather app, styling may be added later

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b2de05a32cmsh76eeaff4d666cedp1e167fjsnb0a300186265",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "ad0ce3a11c90b1ba90ccdd327f63448e";
